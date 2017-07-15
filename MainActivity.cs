cfc1edaed70dfe8a41028c81eeb1e74dea308849
fahimjisan <fahimjisan@yahoo.com>
2017-07-15 13:12:12 -0400

    Add project files.

---
+++ MainActivity.cs
@@ -0,0 +1,22 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+
+namespace XamarinFirebase
+{
+    [Activity(Label = "XamarinFirebase", MainLauncher = true, Icon = "@drawable/icon", Theme = "@style/AppTheme")]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.Main);
+        } public override bool  OnCreateOptionsMenu (IMenu menu)
+        { ManuInflater.Inflate(Resource.Menu.menu_main, menu);
+            return base.OnCreateOptionsMenu(menu);
+        }
+    }
+}
+  
